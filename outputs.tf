01fe46a5b76725518059373905b68d7d609b4fe6
Abhi Yerra <abhi@opszero.com>
2024-08-28 01:40:17 -0700

    Remove

--- outputs.tf
+++ outputs.tf
@@ -12,18 +12,3 @@ output "redis_arn" {
   value       = length(aws_elasticache_replication_group.cluster) > 0 ? aws_elasticache_replication_group.cluster[0].arn : length(aws_elasticache_replication_group.cluster) > 0 ? aws_elasticache_replication_group.cluster[0].arn : ""
   description = "Redis arn"
 }
-
-output "sg_id" {
-  value = aws_security_group.default[*].id
-}
-
-# output "redis_ssm_name" {
-#   value       = aws_ssm_parameter.secret-endpoint[*].name
-#   description = "A list of all of the parameter values"
-# }
-
-# output "auth_token" {
-#   value       = random_password.auth_token[0].result
-#   sensitive   = true
-#   description = "Auth token generated value"
-# }
